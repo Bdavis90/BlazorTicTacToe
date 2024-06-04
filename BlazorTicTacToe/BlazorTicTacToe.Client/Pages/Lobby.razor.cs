5e76db22e9c4be965d635b111fd39836ad2205a7
Brandon Davis <wowzerss90@yahoo.com>
2024-06-04 15:09:19 -0400

    Setup boiler plate code for running signalR

---
+++ BlazorTicTacToe/BlazorTicTacToe.Client/Pages/Lobby.razor.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BlazorTicTacToe.Client.Pages
+{
+    public partial class Lobby 
+    {
+        private HubConnection? hubConnection;
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl(NavigationManager.ToAbsoluteUri("/gamehub"))
+                .Build();
+
+            await hubConnection.StartAsync();
+        }
+    }
+}
